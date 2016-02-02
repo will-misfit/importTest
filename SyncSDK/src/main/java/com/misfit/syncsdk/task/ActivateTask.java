bf53c0671137a932d0cc154caebc78270aad4e9c
WillHou <xiaogang@misift.com>
2016-02-02 22:50:52 +0800

    finished ActivateTask

--- SyncSDK/src/main/java/com/misfit/syncsdk/task/ActivateTask.java
+++ SyncSDK/src/main/java/com/misfit/syncsdk/task/ActivateTask.java
@@ -1,9 +1,19 @@
 package com.misfit.syncsdk.task;
 
-/**
- * Created by Will Hou on 1/13/16.
- */
-public class ActivateTask extends Task {
+import android.util.Log;
+
+import com.misfit.ble.shine.ActionID;
+import com.misfit.ble.shine.ShineProfile;
+import com.misfit.ble.shine.ShineProperty;
+import com.misfit.syncsdk.ConnectionManager;
+import com.misfit.syncsdk.ShineSdkProfileProxy;
+import com.misfit.syncsdk.utils.MLog;
+
+import java.util.Hashtable;
+
+public class ActivateTask extends Task implements ConnectionManager.ConfigCompletedCallback {
+
+    private final static String TAG = "ActivateTask";
 
     @Override
     protected void prepare() {
@@ -12,16 +22,39 @@ public class ActivateTask extends Task {
 
     @Override
     protected void execute() {
-
+        ShineSdkProfileProxy proxy = ConnectionManager.getInstance().getShineSDKProfileProxy(mTaskSharedData.getSerialNumber());
+        if (proxy == null || !proxy.isConnected()) {
+            taskFailed("proxy not prepared");
+            return;
+        }
+        if (mTaskSharedData.getSyncParams() == null) {
+            taskSucceed();
+            return;
+        }
+        ConnectionManager.getInstance().subscribeConfigCompleted(mTaskSharedData.getSerialNumber(), this);
+        proxy.startActivating();
     }
 
     @Override
-    protected void onStop() {
+    public void onStop() {
 
     }
 
     @Override
     protected void cleanup() {
+        ConnectionManager.getInstance().unsubscribeConfigCompleted(mTaskSharedData.getSerialNumber(), this);
+    }
 
+    @Override
+    public void onConfigCompleted(ActionID actionID, ShineProfile.ActionResult resultCode, Hashtable<ShineProperty, Object> data) {
+        if (actionID == ActionID.ACTIVATE) {
+            if (resultCode == ShineProfile.ActionResult.SUCCEEDED) {
+                taskSucceed();
+            } else {
+                retry();
+            }
+        } else {
+            MLog.d(TAG, "unexpected action=" + actionID + ", result=" + resultCode);
+        }
     }
 }
