4aa9856c3d6d959877fea63df2d9a3f63e98c05f
WillHou <xiaogang@misift.com>
2016-02-02 15:57:12 +0800

    add some default connection parameters(Shine2/ Ray/ IWC)

--- SyncSDK/src/main/java/com/misfit/syncsdk/ConnectionParameterManager.java
+++ SyncSDK/src/main/java/com/misfit/syncsdk/ConnectionParameterManager.java
@@ -8,8 +8,18 @@ import com.misfit.ble.shine.ShineConnectionParameters;
 public class ConnectionParameterManager {
 
     public static ShineConnectionParameters defaultParams() {
-        ShineConnectionParameters params = new ShineConnectionParameters(7.5, 0, 720);
-        return params;
+        return new ShineConnectionParameters(7.5, 0, 720);
     }
 
+    public static ShineConnectionParameters defaultShine2Params() {
+        return new ShineConnectionParameters(15, 0, 720);
+    }
+
+    public static ShineConnectionParameters defaultRayParams() {
+        return new ShineConnectionParameters(15, 0, 720);
+    }
+
+    public static ShineConnectionParameters defaultIwcParams() {
+        return new ShineConnectionParameters(15, 0, 720);
+    }
 }
