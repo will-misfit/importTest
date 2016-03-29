3b7b19d4cc9e73290dab0fb43473dcb6b4bc237f
bruyu <bruce@misfit.com>
2016-03-29 17:16:34 +0800

    [SYNC-1022]: add toString() to PlutoSequence.LED/Vibe/Sound

--- ShineSDK/src/main/java/com/misfit/ble/setting/pluto/PlutoSequence.java
+++ ShineSDK/src/main/java/com/misfit/ble/setting/pluto/PlutoSequence.java
@@ -1,49 +1,85 @@
 package com.misfit.ble.setting.pluto;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by Quoc-Hung Le on 8/27/15.
  */
 public class PlutoSequence {
 
-	public static class LED {
-		public static final short DEFAULT_SEQUENCE = 0;
+    public static class LED {
+        public static final short DEFAULT_SEQUENCE = 0;
 
-		private short mValue;
+        private short mValue;
 
-		public short getValue() {
-			return mValue;
-		}
+        public short getValue() {
+            return mValue;
+        }
 
-		public LED(short mValue) {
-			this.mValue = mValue;
-		}
-	}
+        public LED(short mValue) {
+            this.mValue = mValue;
+        }
 
-	public static class Vibe {
-		public static final short DEFAULT_SEQUENCE = 3;
+        @Override
+        public String toString() {
+            JSONObject valueJSON = new JSONObject();
+            try {
+                valueJSON.put("value", mValue);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return valueJSON.toString();
+        }
+    }
 
-		private short mValue;
+    public static class Vibe {
+        public static final short DEFAULT_SEQUENCE = 3;
 
-		public short getValue() {
-			return mValue;
-		}
+        private short mValue;
 
-		public Vibe(short mValue) {
-			this.mValue = mValue;
-		}
-	}
+        public short getValue() {
+            return mValue;
+        }
 
-	public static class Sound {
-		public static final short DEFAULT_SEQUENCE = 1;
+        public Vibe(short mValue) {
+            this.mValue = mValue;
+        }
 
-		private short mValue;
+        @Override
+        public String toString() {
+            JSONObject valueJSON = new JSONObject();
+            try {
+                valueJSON.put("value", mValue);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return valueJSON.toString();
+        }
+    }
 
-		public short getValue() {
-			return mValue;
-		}
+    public static class Sound {
+        public static final short DEFAULT_SEQUENCE = 1;
 
-		public Sound(short mValue) {
-			this.mValue = mValue;
-		}
-	}
+        private short mValue;
+
+        public short getValue() {
+            return mValue;
+        }
+
+        public Sound(short mValue) {
+            this.mValue = mValue;
+        }
+
+        @Override
+        public String toString() {
+            JSONObject valueJSON = new JSONObject();
+            try {
+                valueJSON.put("value", mValue);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return valueJSON.toString();
+        }
+    }
 }
