62a7acbaffb807178b957b961dda653d187f9836
WillHou <xiaogang@misift.com>
2016-04-01 12:58:40 +0800

    [NoJira] add IntDef

--- SyncSDK/src/main/java/com/misfit/syncsdk/enums/SdkUnit.java
+++ SyncSDK/src/main/java/com/misfit/syncsdk/enums/SdkUnit.java
@@ -1,6 +1,11 @@
 package com.misfit.syncsdk.enums;
 
+import android.support.annotation.IntDef;
+
 import com.misfit.cloud.algorithm.models.UnitSystemShine;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 /**
  * a data model to reflect ActivityChangeTagShine in algorithm library namespace
@@ -10,6 +15,10 @@ public class SdkUnit {
 
 	public final static int WEIGHT_UNIT_US = 0;
 	public final static int WEIGHT_UNIT_SI = 1;
+
+	@IntDef({WEIGHT_UNIT_US, WEIGHT_UNIT_SI})
+	@Retention(RetentionPolicy.SOURCE)
+	public @interface WeightUnit {}
 
 	private SdkUnit(){}
 
