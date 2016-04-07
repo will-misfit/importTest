4e330d1c16b529662151cd42bc5b8e0ddef027e0
WillHou <xiaogang@misift.com>
2016-04-07 17:27:48 +0800

    [NoJira] code refactoring

--- SyncSDK/src/main/java/com/misfit/syncsdk/task/SyncAndCalculateTask.java
+++ SyncSDK/src/main/java/com/misfit/syncsdk/task/SyncAndCalculateTask.java
@@ -189,95 +189,98 @@ public class SyncAndCalculateTask extends Task implements ShineProfile.SyncCallb
 
         @Override
         protected Void doInBackground(Void... params) {
-            syncResult = sortRawData(rawSyncDataList);
-            filterRawData(syncResult, mTaskSharedData.getSyncParams().lastSyncTime);
-            buildActivitySessionGroup(syncResult);
+            sortRawData();
+            filterRawData();
+            saveMisfitSyncData(syncResult);
             return null;
+        }
+
+        /**
+         * sort the raw data
+         */
+        private void sortRawData() {
+            Log.d(TAG, "sortRawData()");
+            if (CheckUtils.isCollectionEmpty(rawSyncDataList)) {
+                return;
+            }
+            AlgorithmUtils.sortSyncResultList(rawSyncDataList);
+            for (int i = 1; i < rawSyncDataList.size(); i++) {
+                AlgorithmUtils.handleNotContinuousActivities(rawSyncDataList.get(i - 1), rawSyncDataList.get(i));
+            }
+            syncResult = AlgorithmUtils.mergeSyncResults(rawSyncDataList);
+        }
+
+        /**
+         * filter the raw data with lastSyncTime
+         */
+        private void filterRawData() {
+            Log.d(TAG, "filterRawData()");
+            long lastSyncTime = mTaskSharedData.getSyncParams().lastSyncTime;
+
+            if (syncResult.mActivities.isEmpty()) {
+                return;  // if no data in syncResult.mActivities, nothing to filter
+            }
+            final int n = syncResult.mActivities.size();
+
+            // if lastSyncTime is later than current tail activity's start time, nothing to filter
+            long tailActivityStartTime = syncResult.mActivities.get(n - 1).mStartTimestamp;
+            if (tailActivityStartTime < lastSyncTime) {
+                MLog.d(TAG, "No data is newer than last data synced, do not import those activities");
+                return;
+            }
+
+            if (lastSyncTime == 0l) {
+                MLog.d(TAG, "Last sync time has not been saved before, so that nothing to filter");
+                return;
+            }
+            AlgorithmUtils.filterSyncResultInternalData(syncResult, lastSyncTime);
+        }
+
+        private void saveMisfitSyncData(SyncResult syncResult) {
+            MLog.d(TAG, String.format("saveMisfitSyncData(), syncResult size %d", syncResult.mActivities.size()));
+
+            if (syncResult != null && !CheckUtils.isCollectionEmpty(syncResult.mActivities)) {
+                boolean supportActivityTagging = mTaskSharedData.supportSettingsElement(
+                        SettingsElement.ACTIVITY_TAGGING);
+                boolean supportStream = mTaskSharedData.isStreamingSupported();
+
+                if (mTaskSharedData.getDeviceType() == DeviceType.FLASH) {
+                    if (!CheckUtils.isCollectionEmpty(syncResult.mSessionEvents) && !supportActivityTagging) {
+                        MLog.d(TAG, String.format("Device do not support activity tagging, tags: %d", syncResult.mSessionEvents.size()));
+                        syncResult.mSessionEvents.clear();
+                    }
+                    SdkActivitySessionGroup sdkActivitySessionGroup = DailyUserDataBuilder.getInstance().buildUserDataForFlash(syncResult,
+                            mTaskSharedData.getSyncParams().settingsChangeListSinceLastSync,
+                            mTaskSharedData.getSyncParams().userProfile);
+                    if (mTaskSharedData.getReadDataCallback() != null) {
+                        PostCalculateData postCalculateData = mTaskSharedData.getReadDataCallback().onDataCalculateCompleted(sdkActivitySessionGroup);
+                        mTaskSharedData.setPostCalculateData(postCalculateData);
+                    }
+                } else {
+                    if (!CheckUtils.isCollectionEmpty(syncResult.mTapEventSummarys)) {
+                        if (!supportActivityTagging && !supportStream) {
+                            MLog.d(TAG, String.format("Device do not support activity tagging and streaming, tags: %d", syncResult.mTapEventSummarys.size()));
+                            syncResult.mTapEventSummarys.clear();
+                        }
+                    }
+
+                    SdkActivitySessionGroup sdkActivitySessionGroup = DailyUserDataBuilder.getInstance().buildUserDataForShine(syncResult,
+                            mTaskSharedData.getSyncParams().settingsChangeListSinceLastSync,
+                            mTaskSharedData.getSyncParams().userProfile);
+                    mLogEvent.end(LogEvent.RESULT_SUCCESS, "");
+                    if (mTaskSharedData.getReadDataCallback() != null) {
+                        PostCalculateData postCalculateData = mTaskSharedData.getReadDataCallback().onDataCalculateCompleted(sdkActivitySessionGroup);
+                        mTaskSharedData.setPostCalculateData(postCalculateData);
+                    }
+                }
+                mLogEvent.end(LogEvent.RESULT_SUCCESS, "ActivitySessionGroup is built up");
+            }
         }
 
         @Override
         protected void onPostExecute(Void result) {
             super.onPostExecute(result);
             taskSucceed();
-        }
-    }
-
-    /**
-     * sort the raw SyncResult list
-     */
-    public static SyncResult sortRawData(List<SyncResult> rawSyncDataList) {
-        Log.d(TAG, "sortRawData()");
-        if (CheckUtils.isCollectionEmpty(rawSyncDataList)) {
-            return new SyncResult();
-        }
-        AlgorithmUtils.sortSyncResultList(rawSyncDataList);
-        for (int i = 1; i < rawSyncDataList.size(); i++) {
-            AlgorithmUtils.handleNotContinuousActivities(rawSyncDataList.get(i - 1), rawSyncDataList.get(i));
-        }
-        return AlgorithmUtils.mergeSyncResults(rawSyncDataList);
-    }
-
-    /**
-     * filter the raw data with lastSyncTime
-     * @param syncResult, inout param
-     */
-    public static void filterRawData(SyncResult syncResult, long lastSyncTime) {
-        if (syncResult.mActivities.isEmpty()) {
-            return;
-        }
-        final int n = syncResult.mActivities.size();
-
-        // if lastSyncTime is later than current tail activity's start time, nothing to filter
-        long tailActivityStartTime = syncResult.mActivities.get(n - 1).mStartTimestamp;
-        if (tailActivityStartTime < lastSyncTime) {
-            MLog.d(TAG, "No data is newer than last data synced, do not import those activities");
-            return;
-        }
-
-        if (lastSyncTime == 0l) {
-            MLog.d(TAG, "Last sync time has not been saved before, so that nothing to filter");
-            return;
-        }
-        AlgorithmUtils.filterSyncResultInternalData(syncResult, lastSyncTime);
-    }
-
-    public void buildActivitySessionGroup(SyncResult syncResult) {
-        MLog.d(TAG, String.format("buildActivitySessionGroup(), syncResult size %d", syncResult.mActivities.size()));
-
-        if (syncResult != null && !CheckUtils.isCollectionEmpty(syncResult.mActivities)) {
-            boolean supportActivityTagging = mTaskSharedData.supportSettingsElement(
-                    SettingsElement.ACTIVITY_TAGGING);
-            boolean supportStream = mTaskSharedData.isStreamingSupported();
-
-            if (mTaskSharedData.getDeviceType() == DeviceType.FLASH) {
-                if (!CheckUtils.isCollectionEmpty(syncResult.mSessionEvents) && !supportActivityTagging) {
-                    MLog.d(TAG, String.format("Device do not support activity tagging, tags: %d", syncResult.mSessionEvents.size()));
-                    syncResult.mSessionEvents.clear();
-                }
-                SdkActivitySessionGroup sdkActivitySessionGroup = DailyUserDataBuilder.getInstance().buildUserDataForFlash(syncResult,
-                    mTaskSharedData.getSyncParams().settingsChangeListSinceLastSync,
-                    mTaskSharedData.getSyncParams().userProfile);
-                if (mTaskSharedData.getReadDataCallback() != null) {
-                    PostCalculateData postCalculateData = mTaskSharedData.getReadDataCallback().onDataCalculateCompleted(sdkActivitySessionGroup);
-                    mTaskSharedData.setPostCalculateData(postCalculateData);
-                }
-            } else {
-                if (!CheckUtils.isCollectionEmpty(syncResult.mTapEventSummarys)) {
-                    if (!supportActivityTagging && !supportStream) {
-                        MLog.d(TAG, String.format("Device do not support activity tagging and streaming, tags: %d", syncResult.mTapEventSummarys.size()));
-                        syncResult.mTapEventSummarys.clear();
-                    }
-                }
-
-                SdkActivitySessionGroup sdkActivitySessionGroup = DailyUserDataBuilder.getInstance().buildUserDataForShine(syncResult,
-                    mTaskSharedData.getSyncParams().settingsChangeListSinceLastSync,
-                    mTaskSharedData.getSyncParams().userProfile);
-                if (mTaskSharedData.getReadDataCallback() != null) {
-                    PostCalculateData postCalculateData = mTaskSharedData.getReadDataCallback().onDataCalculateCompleted(sdkActivitySessionGroup);
-                    mTaskSharedData.setPostCalculateData(postCalculateData);
-                }
-            }
-            mLogEvent.end(LogEvent.RESULT_SUCCESS, "ActivitySessionGroup is built up");
         }
     }
 }
